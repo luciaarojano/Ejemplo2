e3d8b2543374a4eb09efc391e27a9888b699cfc8
luciaarojano <131156379+luciaarojano@users.noreply.github.com>
2026-02-11 09:06:26 +0100

    Estructura de una Clase
    Diferentes secciones de la clase con una breve descripción de su funcionalidad

--- Ejemplo2/MiClase.cs
+++ Ejemplo2/MiClase.cs
@@ -8,6 +8,18 @@ namespace Ejemplo2
 {
     public class MiClase
     {
+        //CONSTANTES - ASPECTOS DE CONFIGURACIÓN DE LA CLASE
 
+        //MIEMBROS/CAMPOS PRIVADOS - AGRUPAR DATOS PROTEGIDOS (NO ACCESIBLES DESDE FUERA DE LA CLASE)
+        //NOMENCLATURA: _nombredelCampo
+
+        //CONSTRUCTORES - MÉTODOS ESPECIALES QUE DEFINEN LA FORMA DE INSTANCIAR OBJETOS DE LA CLASE
+        //Si no se definen constructores, la clase facilita uno por defecto - MiClase()
+
+        //PROPIEDADES - INTERFAZ QUE OFRECE LA CLASE PARA CONTROLAR EL ACCESO A LOS CAMPOS PROTEGIDOS
+        //Implementa los métodos get y set para obtener y establecer los "datos asociados"
+
+        //MÉTODOS PÚBLICOS Y PRIVADOS 
+        
     }
 }
